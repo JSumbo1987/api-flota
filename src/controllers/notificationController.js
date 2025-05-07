da4e0a873ba30ba9be07db125f47473a9f751cbc
JBaptista <jbaptista.macosso@gmail.com>
2025-05-07 08:39:18 +0100

    Update Welcome Page

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -1,9 +1,4 @@
-const path = require("path");
-const fs = require('fs');
 const { checkDocumentExpirations } = require("../services/notificationService");
-
- //Carregar Ficheiros HTML.
-  let welcome = fs.readFileSync(path.join(__dirname, "../upload/welcome.html"), 'utf-8');
 
 const checkDocumentos = async(req, res)=>{
     try {
@@ -13,12 +8,4 @@ const checkDocumentos = async(req, res)=>{
     }
 };
 
-const welcomeRoute = async(req, res)=>{
-    try {
-        res.sendFile(__dirname + '/welcome.html');
-    } catch (error) {
-        return res.status(500).json({message: 'Erro ao encaminhar para rota padrão, por favor verifique a console.', error});
-    }
-};
-
-module.exports = { checkDocumentos, welcomeRoute};
+module.exports = { checkDocumentos };
